41003b3b378628bd3d77ada5d11a4858c569865a
Jason <jasonstrain00@gmail.com>
2019-12-08 01:54:48 +0000

    Initial commit

---
+++ static/js/chart.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+ 
+var dps = [];
+    
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Email Categories",
+		horizontalAlign: "left"
+	},
+	data: [{
+		type: "doughnut",
+		startAngle: 60,
+		//innerRadius: 60,
+		indexLabelFontSize: 17,
+		indexLabel: "{label} - #percent%",
+		toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+		dataPoints: dps
+	}]
+});
+chart.render();
+ 
+} 
